b1586171ecc9fe4499c61f3d465e9a853dbe4a63
Vishnu Kumar <p23vkumar@fairfieldprep.org>
2022-10-03 14:10:15 -0400

    Lab 3-4
    Lab 3-4

---
+++ lab 3-4.py
@@ -0,0 +1,46 @@
+#1. Create a Python file named lab_3-4.py
+#2. Without using Python, determine the value of expressions. Use a comment to
+#record your determined value
+#1. math.ceil(7.15)
+#2. math.floor(19.156)
+#3. math.fabs(-42)
+#4. math.pow(2, 3)
+#5. math.log10(100)
+#6. math.sqrt(9)
+#7. math.isqrt(2)
+#8. math.factorial(5)
+#9. math.gcd(111111111, 12321)
+#10. math.sin(0)
+#Copy the syntax into your file and check your answers.
+
+import math
+
+a = math.ceil(7.15)
+print(a)
+
+x = math.floor(19.156)
+print(x)
+
+y = math.fabs(-42)
+print(y)
+
+z = math.pow(2, 3)
+print(z)
+
+l = math.log10(100)
+print(l)
+
+h = math.sqrt(9)
+print(h)
+
+e = math.isqrt(2)
+print(e)
+
+r = math.factorial(5)
+print(r)
+
+d = math.gcd(111111111, 12321)
+print(d)
+
+b = math.sin(0)
+print(b)
